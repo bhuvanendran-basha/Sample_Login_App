65bad6181c48be5e0ea670573c1ac8d649b27717
bhuva <bhuva@host.docker.internal>
2021-07-08 16:02:51 +0530

    first commit

---
+++ SpringBoot-master/src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.postgresdemo;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PostgresDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDemoApplication.class, args);
+	}
+	
+	/**
+	 * Hibernate configuration with spring boot 
+	 * @param emf
+	 * @return SessionFactory
+	 */
+	@Bean
+	public SessionFactory sessionFactory(EntityManagerFactory emf) {
+	    return emf.unwrap(SessionFactory.class);
+	}
+}
